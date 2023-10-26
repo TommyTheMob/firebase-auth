f6a5eb56d323cf4e7ba375b620af95b26a0e5f8a
TommyTheMob <zv3rbrus@gmail.com>
2023-10-26 16:02:58 +0300

    add AppNavbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
+import AppNavbar from "./shared/AppNavbar.jsx";
 
 function App() {
 
   return (
     <>
+        <AppNavbar />
         <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/login' element={<LoginPage />} />
---
+++ src/shared/AppNavbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Link, Outlet} from "react-router-dom";
+import logo from '../assets/firebase-icon.svg'
+
+const AppNavbar = () => {
+    return (
+        <>
+            <Navbar expand='lg' className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand as={Link} to='/'>
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Firebase
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet />
+        </>
+    );
+};
+
+export default AppNavbar;
